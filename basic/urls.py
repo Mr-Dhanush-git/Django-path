3e9b949f9d5156e5ef9df752e2932b06d2ee8b43
Mr-Dhanush-git <dhanushchinthalapudi@gmail.com>
2026-09-18 19:27:19 +0530

    implemented search , login and logout

--- basic/urls.py
+++ basic/urls.py
@@ -5,6 +5,10 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('login/' , views.loginPage, name = "login"),
+
+    path('logout/' , views.logoutUser, name = "logout"),
+
     path('',views.home , name= "home"),
 
     path('room/<str:pk>/' , views.room , name="room"),
